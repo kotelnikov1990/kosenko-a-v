bc3d8748bc3ad337178dfb93478ec5f8659dcf79
kotelnikov1990 <118100014+kotelnikov1990@users.noreply.github.com>
2022-11-14 21:15:05 +0500

    Add files via upload

---
+++ page.php
@@ -0,0 +1,257 @@
+<?php
+
+/*
+if( SITEPATH === "http://online-shkola.ru/") {
+
+	require "lessphp/lessc.inc.php";
+
+		$main_less = new lessc;
+		$main_less->compileFile("css/less-dd.less", "css/dd.css");
+
+}
+*/
+
+
+?>
+<!DOCTYPE html>
+	<html lang="ru">
+
+	<head>
+
+		<base href="<?=SITEURL;?>" />
+
+		<meta charset="UTF-8">
+		 <title>Гламурненько.Ру. Мода, стиль, красота, имидж</title>
+		<meta name="description" content="Сайт Екатерины Маляровой – имиджмейкера, создателя тренингов по имиджу и стилю. Наша миссия: «Миллион красиво одетых женщин». Обучаем  имиджу с 2005 года." />
+
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+      	<meta http-equiv="Cache-Control" content="private">
+
+		<link rel="stylesheet" type="text/css" href="css/dd.css?<?=$rande?>" />
+		<link rel="stylesheet" type="text/css" href="css/jquery.fancybox.min.css">
+
+
+	</head>
+
+<body>
+<div id="bodyContainer" class="bodyContainer">
+
+		<ul id="navContainer" class="navContainer">
+			<li class="nav-box"><a href="#" class="link">Блог</a></li>
+			<li class="nav-box"><a href="#" class="link">Блог для женщин</a></li>
+			<li class="nav-box"><a href="#" class="link">Блог обувь</a></li>
+			<li class="nav-box"><a href="#" class="link">Блог белье</a></li>
+			<li class="nav-box"><a href="#" class="link">Блог, рюкзаки.</a></li>
+			<li class="nav-box"><a href="#" class="link">Блог текстиль, тюль</a></li>
+			<li class="nav-box"><a href="#" class="link">Блог до 45%</a></li>
+			<li class="nav-sozseti">
+				<a href="#" class="icon-oo"></a>
+				<a href="#" class="icon-ww"></a>
+				<a href="#" class="icon-ff"></a>
+			</li>
+			<li class="nav-tel">123-45-67<br />123-45-67</li>
+		</ul>
+			<div class="logotip">
+						<div class="logo-new"></div>
+			</div>     <!-- //bg-main -->
+
+
+<div class="section">
+	<div class="section-top">
+		<div class="poloska">Новинка</div>
+           <h1 class="center">
+                 Это всего лишь текст чтоб показать расположение текст
+           </h1>
+
+			<ul class="ul-text">
+			<li>
+Это всего лишь текст чтоб показать расположение текст Это всего лишь текст
+чтоб показать расположение текст чтоб показать
+Это всего лишь текст чтоб показать расположение текст Это всего лишь текст чтоб
+показать расположение текст
+			<li>
+			</ul>
+	</div>
+    <div class="section-bg"></div>
+    <h2 class="center">Как все происходит</h2>
+	<ul class="ul-text">
+			<li class="li-text">Это всего лишь текст чтоб показать расположен
+			ие текст Это всего лишь текст </li>
+			<li class="li-text">Это всего лишь текст чтоб показать расположен
+			ие текст Это всего лишь текст Это всего лишь текст чтоб показать расположен
+			</li>
+			<li class="li-text">Это всего лишь текст чтоб показать расположен
+			ие текст Это всего лишь текст </li>
+			<li class="li-text">Это всего лишь текст чтоб показать расположен
+			ие текст Это всего лишь текст Это всего лишь текст чтоб показать расположен
+			ие текст</li>
+			<li class="li-text" style="padding-left:60px">Это всего лишь текст чтоб показать расположен
+			ие текст Это всего лишь текст Это всего лишь текст чтоб показать расположен
+			текст Это всего лишь текст чтоб показать расположен
+			ие текст Это всего лишь текст показать расположен
+			ие текст Это всего лишь текст чтоб показать расположен
+			ие текст Это всего лишь текст показать расположен
+			ие текст Это всего лишь текст чтоб показать расположен
+			ие текс</li>
+	</ul>
+	<div class="zapis-cont">
+			<table class="iksweb">
+				<tbody>
+					<tr style="background:#ffd100">
+						<td style="text-align:left">ВЫ ПОЛУЧАЕТЕ</td>
+						<td class="dlinabox">ТЕОРИЯ</td>
+						<td class="dlinabox">ПРАКТИКА</td>
+						<td class="dlinabox">VIP</td>
+					</tr>
+					<tr>
+						<td style="text-align:left">Все материалы тренинга</td>
+						<td class="dlinabox"><img src="images/s5.png" /></td>
+						<td class="dlinabox"><img src="images/s5.png" /></td>
+						<td class="dlinabox"><img src="images/s5.png" /></td>
+					</tr>
+					<tr>
+						<td style="text-align:left"></td>
+						<td class="dlinabox"></td>
+						<td class="dlinabox"> 2.5 месяца<br /><p>Это всего лишь текст чтоб пока</p></td>
+						<td class="dlinabox"> 2.5 месяца<br /><p>Это всего лишь текст</p></td>
+					</tr>
+					<tr>
+						<td style="text-align:left"></td>
+						<td class="dlinabox"></td>
+						<td class="dlinabox"><img src="images/s5.png" /></td>
+						<td class="dlinabox"><img src="images/s5.png" /></td>
+					</tr>
+					<tr>
+						<td style="text-align:left"></td>
+						<td class="dlinabox"></td>
+						<td class="dlinabox"><img src="images/s5.png" /></td>
+						<td class="dlinabox"><img src="images/s5.png" /></td>
+					</tr>
+					<tr style="background:#ffd100">
+						<td style="text-align:left">ЦЕНА</td>
+						<td>9999 &#8381</td>
+						<td>9999 &#8381</td>
+						<td>9999 &#8381</td>
+					</tr>
+					<tr>
+						<td>
+
+						</td>
+						<td colspan="3"  class="valuta">
+							<div  class="valuta-box">
+							<span>&#8381</span>
+							<span>&#8381</span>
+							<span>&#8381</span>
+							<span>&#8381</span>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td class="dlinabox"><input type="buttom" value="заказать" class="knopka"></td>
+						<td class="dlinabox"><input type="buttom" value="заказать" class="knopka"></td>
+						<td class="dlinabox"><input type="buttom" value="заказать" class="knopka"></td>
+					</tr>
+				</tbody>
+			</table>
+	</div>
+
+	<div class="kniga"></div>
+ <h3 class="center">Записывайтесь прямо сейчас</h3>
+	<div class="zapis-cont-2">
+
+			<div class="zapis-section-2">
+				<ul class="zapis-box-2">
+
+					<li><p class="center fs23">Пакет</p>
+					<p class="center fs27">&laquo;Пробовать&raquo;</p></li>
+					 <li class="fl">Cрок:</li>
+				    <li class="fl"><strong>Это всего лишь</strong></li>
+				    <li class="fl">Вопросов комплектов на оценку</li>
+				    <li class="fl"><strong>5 ваших луков или вопросов</strong></li>
+				    <li class="center fs21 ser" style="padding:15px 0;text-decoration: line-through;">990 руб.</li>
+				    <li class="center fs27" style="padding:0 0 15px 0">433 руб.</li>
+				    <li class="center"><input type="buttom" value="заказать" class="knopka-2"></li>
+				</ul>
+				<ul class="zapis-box-2">
+
+					<li><p class="center fs23">Пакет</p>
+					<p class="center fs27">&laquo;Пробовать&raquo;</p></li>
+					 <li class="fl">Cрок:</li>
+				    <li class="fl"><strong>Это всего лишь</strong></li>
+				    <li class="fl">Вопросов комплектов на оценку</li>
+				    <li class="fl"><strong>5 ваших луков или вопросов</strong></li>
+				    <li class="center fs21 ser" style="padding:15px 0;text-decoration: line-through;">990 руб.</li>
+				    <li class="center fs27" style="padding:0 0 15px 0">433 руб.</li>
+				    <li class="center"><input type="buttom" value="заказать" class="knopka-2"></li>
+				</ul>
+				<ul class="zapis-box-2">
+
+					<li><p class="center fs23">Пакет</p>
+					<p class="center fs27">&laquo;Пробовать&raquo;</p></li>
+					 <li class="fl">Cрок:</li>
+				    <li class="fl"><strong>Это всего лишь</strong></li>
+				    <li class="fl">Вопросов комплектов на оценку</li>
+				    <li class="fl"><strong>5 ваших луков или вопросов</strong></li>
+				    <li class="center fs21 ser" style="padding:15px 0;text-decoration: line-through;">990 руб.</li>
+				    <li class="center fs27" style="padding:0 0 15px 0">433 руб.</li>
+				    <li class="center"><input type="buttom" value="заказать" class="knopka-2"></li>
+				</ul>
+			</div>
+	</div>
+    <h3 class="center">Как все происходит</h3>
+
+    <div class="stilistContainer">
+			    <div class="stilist-cont">
+			    		<a href="images/a1.jpg" class="stilist-icon" data-fancybox="images"><img src="images/a1.jpg" alt=""/></a>
+			    		<div class="stilist-text">Это всего лишь текст чтоб показать расположение текста
+			    		Это всего лишь текст чтоб показать расположение текста
+			    		Это всего лишь текст расположение текста
+			    		Это всего лишь текст чтоб показать расположение текста</div>
+			    </div>
+			    <div class="stilist-cont">
+			    		<a href="images/a2.jpg" class="stilist-icon" data-fancybox="images"><img src="images/a2.jpg" alt=""/></a>
+			    		<div class="stilist-text">Это всего лишь текст чтоб показать расположение текста
+			    		Это всего лишь текст чтоб показать расположение текста
+			    		Это всего лишь текст чтоб показать расположение текста
+			    		Это всего лишь текст чтоб показать расположение текста</div>
+			    </div>
+			    <div class="stilist-cont">
+			    		<a href="images/a2.jpg" class="stilist-icon" data-fancybox="images"><img src="images/a3.jpg" alt=""/></a>
+			    		<div class="stilist-text">Это всего лишь текст чтоб показать расположение текста
+			    		Это всего лишь текст
+			    		Это всего лишь текст чтоб показать расположение текста
+			    		Это всего лишь текст чтоб показать расположение текста</div>
+			    </div>
+			    <div class="stilist-cont">
+			    		<a href="images/a2.jpg" class="stilist-icon" data-fancybox="images"><img src="images/a4.jpg" alt=""/></a>
+			    		<div class="stilist-text">Это всего лишь текст чтоб показать расположение текста
+			    		Это всего лишь текст чтоб показать расположение текста
+			    		Это всего лишь текст чтоб показать расположение текста</div>
+			    </div>
+    </div>
+</div>  <!-- //section-shop -->
+
+			<div class="footer">
+			             <div class="fl">
+						              <p>Это всего лиш</p>
+						              <p>Это всего лишь текст</p>
+						              <p>Это всего лишь текст чтоб показать расположен</p>
+			             </div>
+			             <div id="upbutton"></div>
+			</div>   <!-- стоп /footer-shop -->
+</div>   <!-- стоп id="bodyContainer" -->
+
+
+<div id="top"></div>
+
+
+
+<script type="text/javascript" src="js/jquery-3.3.0.min.js"></script>
+<script  async type="text/javascript" src="js/main.js?v?<?=$rande?>"></script>
+<script async type="text/javascript" src="js/jquery.fancybox.min.js"></script>
+
+</body>
+</html>
---
+++ php/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//$rande = mt_rand(1, 99999);
+$rande = 'dw2sxq';
+
+define('SITEURL', 'https://' .$_SERVER['HTTP_HOST'] .'/' );
+define('TEMPLATE_PATH', $_SERVER['DOCUMENT_ROOT']);
+
+
+?>
